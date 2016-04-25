bb8ca65559d8b861dc9583c205bf785395aae1c3
Eva <evangelia.fahantidou@mymail.champlain.edu>
2016-04-24 23:51:38 -0400

    ??

---
+++ playingWithFire/Assets/Scripts/Level2WallScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level2WallScript : MonoBehaviour {
+
+    public GameObject vineWalls;
+    public GameObject spawn;
+    public GameObject finish;
+    public Quaternion verticalTilt;
+    bool weStart;
+
+	// Use this for initialization
+	void Start () {
+        weStart = true;
+        verticalTilt = Quaternion.Euler(new Vector3(0, 0, 90));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (weStart == true)
+        {
+            Instantiate(spawn, new Vector3(0f, -4.0f, 0), Quaternion.identity);
+            Instantiate(vineWalls, new Vector3(0f, -4.5f, 0), verticalTilt);
+            Instantiate(vineWalls, new Vector3(5.0f, -0.5f, 0), verticalTilt);
+            Instantiate(vineWalls, new Vector3(7.0f, -0.5f, 0), verticalTilt);
+            Instantiate(vineWalls, new Vector3(-5.0f, -1.5f, 0), verticalTilt);
+            Instantiate(finish, new Vector3(7.0f, -2.0f, 0f), Quaternion.identity);
+            weStart = false;
+        }
+	}
+}
